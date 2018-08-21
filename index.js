903e0f09900eb5101dc387c864d37f24576979ae
Salah Saleh <mr.salah1989@gmail.com>
2018-08-21 19:11:10 +0300

    first test

---
+++ index.js
@@ -0,0 +1,17 @@
+import React from "react";
+import { View, Text } from "react-native";
+// import { Rating, AirbnbRating } from 'react-native-ratings';
+// import {Icons} from "native-components";
+// import Iconss from "native-components";
+export default class Main extends React.Component {
+  render() {
+    // console.l og("hhhhhh", Icons);
+    return (
+      <View
+        style={{ flex: 1, alignContent: "center", justifyContent: "center" }}
+      >
+        <Text>Hiiiiiiiiiiiiiiiiiii</Text>
+      </View>
+    );
+  }
+}
